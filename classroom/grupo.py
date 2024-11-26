c41b6e033914b00cf3b4f293c3773f4d0af4f248
sgutierrezbe <sgutierrezbe@unal.edu.co>
2024-11-26 14:03:43 -0500

    grupo fix

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,25 +1,12 @@
 from classroom.asignatura import Asignatura
 
 class Grupo:
-    grado = "Grado 12" 
+    grado = "Grado 12"  # Set default value for class attribute
 
     def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None, **kwargs):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas is not None else []
         self.listadoAlumnos = estudiantes if estudiantes is not None else []
-
-    def listadoAsignaturas(self, **kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
-
-    def agregarAlumno(self, alumno, lista=None):
-        if lista is None:
-            lista = []
-        lista.append(alumno)
-        self.listadoAlumnos.extend(lista)
-
-    def __str__(self):
-        return f"Grupo de estudiantes: {self._grupo}"
 
     @classmethod
     def asignarNombre(cls, nombre="Grado 6"):
